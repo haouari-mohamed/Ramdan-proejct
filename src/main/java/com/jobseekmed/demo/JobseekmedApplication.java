d25002a0ec75709f2fb4ca7a72e9fdfcac16d46f
haouari-mohamed <haouari-mohamed>
2025-03-04 02:19:05 +0000

    add the conception of my application

---
+++ src/main/java/com/jobseekmed/demo/JobseekmedApplication.java
@@ -0,0 +1,13 @@
+package com.jobseekmed.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobseekmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobseekmedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobseekmed/demo/JobseekmedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobseekmed.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobseekmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
