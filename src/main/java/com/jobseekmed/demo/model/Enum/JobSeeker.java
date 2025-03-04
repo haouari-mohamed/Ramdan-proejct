8d1a88be36d8f2ada6f1fbcddab695e504f09920
haouari-mohamed <haouari-mohamed>
2025-03-04 02:31:18 +0000

    add the jobsekker class

---
+++ src/main/java/com/jobseekmed/demo/model/Enum/JobSeeker.java
@@ -0,0 +1,21 @@
+package com.jobseekmed.demo.model.Enum;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobSeeker {
+
+    private String cv;
+    private List<String> skills;
+
+   
+
+
+}
